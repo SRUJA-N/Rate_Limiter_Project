2798062b26b2ecdaead85ec871df69e6da7741e6
Your Name <you@example.com>
2026-08-26 14:21:58 +0530

    done wiht the refill logic and tokens decrement

--- backend/src/main/java/RateLimter/example/RateLimter/controllers/Controllers.java
+++ backend/src/main/java/RateLimter/example/RateLimter/controllers/Controllers.java
@@ -5,6 +5,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class Controllers {
 
@@ -24,6 +26,22 @@ public class Controllers {
         return stringRedisTemplate.opsForValue().get("user:127.0.0.1:tokens");
     }
 
+    @GetMapping("/redis-decrement")
+    public String decrement(){
+        Long remainingToken=stringRedisTemplate.opsForValue().decrement("user:127.0.0.1:tokens");
+        return "Remaining tokens: "+ remainingToken;
+    }
+    @GetMapping("/redis-increment")
+    public String increment(){
+        Long incrementedToken=stringRedisTemplate.opsForValue().increment("user:127.0.0.1:tokens");
+        return "Increased token" + incrementedToken;
+    }
+
+    @GetMapping("/redis-ttl")
+    public String redisttl(){
+        stringRedisTemplate.opsForValue().set("user:127.0.0.1:tokens","it will be delted after",10, TimeUnit.SECONDS);
+       return "the redis will be deleted in 10 second";
+    }
     @GetMapping("/hello")
     public String print(){
            return "request Accepted";
--- backend/src/main/java/RateLimter/example/RateLimter/services/RateLimiterService.java
+++ backend/src/main/java/RateLimter/example/RateLimter/services/RateLimiterService.java
@@ -1,6 +1,7 @@
 package RateLimter.example.RateLimter.services;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import java.util.*;
 @Service
@@ -12,19 +13,74 @@ public class RateLimiterService {
 
     @Value("${rate.limiter.refill-rate}")
     private int refill;
+    private final StringRedisTemplate stringRedisTemplate;
+    public RateLimiterService(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+
+
 
     private final Map<String,TokenBucket> buckets=new HashMap<>();
-    public boolean access( String userId){
+    public boolean access(String userId) {
 
-        TokenBucket bucket=buckets.get(userId);
+        String tokenKey = "rate-limiter:" + userId + ":tokens";
+        String refillKey = "rate-limiter:" + userId + ":last-refill";
 
-        if(bucket==null){
-            bucket = new TokenBucket(capacity,refill);
-            buckets.put(userId,bucket);
+        String tokens = stringRedisTemplate.opsForValue().get(tokenKey);
 
+        // First request from this user
+        if (tokens == null) {
+
+            stringRedisTemplate.opsForValue()
+                    .set(tokenKey, String.valueOf(capacity));
+
+            stringRedisTemplate.opsForValue()
+                    .set(refillKey, String.valueOf(System.currentTimeMillis()));
+
+            tokens = String.valueOf(capacity);
         }
 
-        return bucket.allowRefil();
+        long availableTokens = Long.parseLong(tokens);
 
+        long lastRefill = Long.parseLong(
+                stringRedisTemplate.opsForValue().get(refillKey)
+        );
+
+        long currentTime = System.currentTimeMillis();
+
+        long timeElapsed = currentTime - lastRefill;
+
+        long secondsPassed = timeElapsed / 1000;
+
+        long tokensToAdd = secondsPassed * refill;
+
+        // Refill
+        if (tokensToAdd > 0) {
+
+            long newTokenCount = Math.min(
+                    capacity,
+                    availableTokens + tokensToAdd
+            );
+
+            stringRedisTemplate.opsForValue()
+                    .set(tokenKey, String.valueOf(newTokenCount));
+
+            stringRedisTemplate.opsForValue()
+                    .set(refillKey, String.valueOf(currentTime));
+
+            availableTokens = newTokenCount;
+        }
+
+        // No token available
+        if (availableTokens <= 0) {
+            return false;
+        }
+
+        // Consume one token
+        stringRedisTemplate.opsForValue()
+                .decrement(tokenKey);
+
+        return true;
     }
 }
